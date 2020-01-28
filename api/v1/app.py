282cac5a713e2ae53a0b3e062e8146760b05e191
Sudeep Raj <sudrocks@gmail.com>
2020-01-28 21:08:27 +0000

    Create handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,10 @@ app.register_blueprint(app_views)
 def close(var=None):
     """ cals storage.close() """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
