e8586c3af97204d10661a1e4ae5497fb2b70a6ab
Banuaksom <banuaksom@gmail.com>
2020-01-29 22:00:48 +0000

    added comments for methods and modules

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" app module """
 from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" index module """
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
