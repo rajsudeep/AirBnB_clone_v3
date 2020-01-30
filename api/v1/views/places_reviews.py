7fbdf9c27bd882b712d7d4ac2854dd93391b650e
Banuaksom <banuaksom@gmail.com>
2020-01-30 01:48:30 +0000

    added comments

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -32,6 +32,7 @@ def get_review(review_id):
 @app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id):
+    """ deletes the review obj """
     obj = storage.get("Review", review_id)
     if not obj:
         abort(404)
@@ -43,6 +44,7 @@ def delete_review(review_id):
 @app_views.route('places/<place_id>/reviews', methods=['POST'],
                  strict_slashes=False)
 def post_review(place_id):
+    """ creates place obj """
     if not storage.get("Place", place_id):
         abort(404)
     req = request.get_json()
@@ -59,6 +61,7 @@ def post_review(place_id):
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def put_review(review_id):
+    """ updates review obj """
     obj = storage.get("Review", review_id)
     if not obj:
         abort(404)
